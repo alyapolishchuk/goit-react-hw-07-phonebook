bd69040c673aba4e81a1e32694903210d8e24d26
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-13 10:56:57 +0300

    с

--- src/redux/contacts/contscts-actions.js
+++ src/redux/contacts/contscts-actions.js
@@ -1,17 +1,18 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { getContacts, postContact, deleteContact } from 'services/api';
 //-------------------------------------------------------------//
+
 export const getUsers = createAsyncThunk('contacts/get', async () => {
   return await getContacts();
 });
 export const addUser = createAsyncThunk('contacts/add', async contact => {
-  await postContact(contact);
-  return await getContacts();
+  const response = await postContact(contact);
+  return response.data;
 });
 
 export const deleteUser = createAsyncThunk('contacts/delete', async id => {
-  await deleteContact(id);
-  return await getContacts();
+  const response = await deleteContact(id);
+  return response.data.id;
 });
 
 export const filterUser = createAction('contacts/filter');
